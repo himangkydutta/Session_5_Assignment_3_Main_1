27f2b30207a0879170915c7d5f66407866630c0b
himangkydutta <himangkydutta08@gmail.com>
2017-07-03 21:53:21 +0530

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+//Adding component
+@Component({
+  selector: 'my-app-component',
+  templateUrl: './app.component.html',
+  styles:'./app.component.css'
+})
+export class LoginComponent {
+
+}
